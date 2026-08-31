a3b6c5879b0ef6123a0c603d5551adc6da209ae9
Nehaa Chowdary <neeha.chowdary@rebl.se>
2026-08-31 14:43:16 +0200

    Add linear_axis constraint to combined lift+carry stage - confirmed smooth motion, ~50% waypoint reduction

---
+++ 04_path_constraints/single_gripper/remove_obstacle.py
@@ -0,0 +1,21 @@
+﻿from isaacsim import SimulationApp
+simulation_app = SimulationApp({"headless": False})
+
+import omni.usd
+from isaacsim.core.api import World
+from isaacsim.core.utils.stage import open_stage
+
+open_stage("C:/Users/NehaaChowdary/Documents/newware_house.usd")
+world = World()
+world.reset()
+stage = omni.usd.get_context().get_stage()
+
+prim = stage.GetPrimAtPath("/World/static_obstacle")
+if prim.IsValid():
+    stage.RemovePrim("/World/static_obstacle")
+    print("Obstacle removed")
+else:
+    print("No obstacle found to remove")
+
+omni.usd.get_context().save_stage()
+simulation_app.close()
